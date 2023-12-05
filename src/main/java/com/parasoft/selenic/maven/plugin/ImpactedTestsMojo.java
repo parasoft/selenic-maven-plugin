026bc5563eff75355097ba121ca622f0d6c4fcd4
Joseph Benken <benken@parasoft.com>
2023-12-05 09:34:51 -0800

    run jtestcov "impacted" command

--- src/main/java/com/parasoft/selenic/maven/plugin/ImpactedTestsMojo.java
+++ src/main/java/com/parasoft/selenic/maven/plugin/ImpactedTestsMojo.java
@@ -16,10 +16,21 @@
 
 package com.parasoft.selenic.maven.plugin;
 
+import static java.lang.System.lineSeparator;
+
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -37,10 +48,72 @@ public class ImpactedTestsMojo extends AbstractMojo {
     @Parameter(property = "selenic.home", defaultValue = "${env.SELENIC_HOME}")
     private File selenicHome;
 
+    /**
+     * Specifies additional JVM options. Example:
+     *
+     * <pre><code>{@literal <vmArgs>}
+     *   {@literal <vmArg>}-Xmx2g{@literal </vmArg>}
+     * {@literal </vmArgs>}</code></pre>
+     */
+    @Parameter(property = "selenic.vmargs")
+    private List<String> vmArgs;
+
     @Override
     public void execute() throws MojoExecutionException {
+        Log log = getLog();
         if (selenicHome == null) {
             throw new MojoExecutionException(Messages.get("selenic.home.not.set")); //$NON-NLS-1$
+        }
+        if (!selenicHome.exists() || !Files.exists(selenicHome.toPath().resolve("selenic_agent.jar")) //$NON-NLS-1$
+                || !Files.exists(selenicHome.toPath().resolve("selenic_analyzer.jar"))) { //$NON-NLS-1$
+            throw new MojoExecutionException(Messages.get("selenic.missing", selenicHome)); //$NON-NLS-1$
+        }
+        Path covtoolJar = selenicHome.toPath().resolve("coverage").resolve("Java").resolve("jtestcov") //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                .resolve("jtestcov.jar"); //$NON-NLS-1$
+        if (!Files.exists(covtoolJar)) {
+            throw new MojoExecutionException(Messages.get("covtool.missing", covtoolJar)); //$NON-NLS-1$
+        }
+        runCovtoolJar(log, covtoolJar);
+    }
+
+    private void runCovtoolJar(Log log, Path covtoolJar) throws MojoExecutionException {
+        String osName = System.getProperty("os.name"); //$NON-NLS-1$
+        boolean isWindows = osName != null && osName.startsWith("Windows"); //$NON-NLS-1$
+        String javaExe = Paths.get(System.getProperty("java.home"), "bin", //$NON-NLS-1$ //$NON-NLS-2$
+                isWindows ? "java.exe" : "java").toFile().getAbsolutePath(); //$NON-NLS-1$ //$NON-NLS-2$
+        List<String> command = new LinkedList<>();
+        command.add(javaExe);
+        if (vmArgs != null) {
+            command.addAll(vmArgs);
+        }
+        command.add("-jar"); //$NON-NLS-1$
+        command.add(covtoolJar.toAbsolutePath().toString());
+        command.add("impacted"); //$NON-NLS-1$
+        command.add("-selenic"); //$NON-NLS-1$
+        runCommand(log, command);
+    }
+
+    private static void runCommand(Log log, List<String> command) throws MojoExecutionException {
+        if (log.isDebugEnabled()) {
+            log.debug("command:" + lineSeparator() + String.join(lineSeparator(), command)); //$NON-NLS-1$
+        }
+        ProcessBuilder pb = new ProcessBuilder(command);
+        pb.inheritIO();
+        try {
+            Process process = pb.start();
+            try (OutputStream out = process.getOutputStream();
+                    InputStream in = process.getInputStream();
+                    InputStream err = process.getErrorStream()) {
+                int exitCode = process.waitFor();
+                if (exitCode != 0) {
+                    throw new MojoExecutionException(Messages.get("covtool.returned.exit.code", exitCode)); //$NON-NLS-1$
+                }
+            } catch (InterruptedException e) {
+                process.destroy();
+                throw new MojoExecutionException(e);
+            }
+        } catch (IOException e) {
+            throw new MojoExecutionException(e);
         }
     }
 }
--- src/test/java/com/parasoft/selenic/maven/plugin/ImpactedTestsMojoTest.java
+++ src/test/java/com/parasoft/selenic/maven/plugin/ImpactedTestsMojoTest.java
@@ -16,15 +16,31 @@
 
 package com.parasoft.selenic.maven.plugin;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.endsWith;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.withSettings;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
 
+import org.apache.commons.lang3.SystemUtils;
 import org.apache.maven.plugin.testing.MojoRule;
 import org.apache.maven.plugin.testing.WithoutMojo;
 import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.MockedConstruction;
 
 public class ImpactedTestsMojoTest {
     @Rule
@@ -44,9 +60,39 @@ public class ImpactedTestsMojoTest {
         assertNotNull(pom);
         assertTrue(pom.exists());
 
-        ImpactedTestsMojo myMojo = (ImpactedTestsMojo) rule.lookupConfiguredMojo(pom, "impacted-tests");
-        assertNotNull(myMojo);
-        myMojo.execute();
+        ImpactedTestsMojo impactedTestsMojo = (ImpactedTestsMojo) rule.lookupConfiguredMojo(pom, "impacted-tests");
+        assertNotNull(impactedTestsMojo);
+        List<String> command;
+        Path mockSelenicInstallation = Files.createTempDirectory("selenic");
+        try {
+            Files.createFile(mockSelenicInstallation.resolve("selenic_agent.jar"));
+            Files.createFile(mockSelenicInstallation.resolve("selenic_analyzer.jar"));
+            Path mockCovtoolInstallation = mockSelenicInstallation.resolve("coverage").resolve("Java").resolve("jtestcov");
+            Files.createDirectories(mockCovtoolInstallation);
+            Files.createFile(mockCovtoolInstallation.resolve("jtestcov.jar"));
+            rule.setVariableValueToObject(impactedTestsMojo, "selenicHome", mockSelenicInstallation.toFile());
+            Process process = mock(Process.class);
+            try (MockedConstruction<ProcessBuilder> processBuilder = mockConstruction(ProcessBuilder.class,
+                    withSettings().defaultAnswer(CALLS_REAL_METHODS), (mock, context) -> {
+                        doReturn(process).when(mock).start();
+                        doReturn(context.arguments().get(0)).when(mock).command();
+                    })) {
+                impactedTestsMojo.execute();
+                List<ProcessBuilder> constructed = processBuilder.constructed();
+                assertEquals(1, constructed.size());
+                command = constructed.get(0).command();
+            }
+        } finally {
+            try (Stream<Path> stream = Files.walk(mockSelenicInstallation)) {
+                stream.sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+            }
+        }
+        assertEquals(5, command.size());
+        assertThat(command.get(0), endsWith(Paths.get("bin", SystemUtils.IS_OS_WINDOWS ? "java.exe" : "java").toString()));
+        assertEquals("-jar", command.get(1));
+        assertThat(command.get(2), endsWith(Paths.get("coverage", "Java", "jtestcov", "jtestcov.jar").toString()));
+        assertEquals("impacted", command.get(3));
+        assertEquals("-selenic", command.get(4));
     }
 
     /** Do not need the MojoRule. */
